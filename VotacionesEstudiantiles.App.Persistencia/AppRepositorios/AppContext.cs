ddd8debac98349ebdf626429ab6c9c721d0346fc
F3l1x2301 <fmarrugop@unicartagena.edu.co>
2022-09-19 22:43:04 -0500

    persistencia 2

--- VotacionesEstudiantiles.App.Persistencia/AppRepositorios/AppContext.cs
+++ VotacionesEstudiantiles.App.Persistencia/AppRepositorios/AppContext.cs
@@ -4,5 +4,15 @@ using VotacionesEstudiantiles.App.Dominio;
 
 namespace VotacionesEstudiantiles.App.Persistencia
 {
+    public class AppContext : DbContext{
 
+        //Metodos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = VotacionesEstudiantiles");
+            }
+        }
+    }
 }
