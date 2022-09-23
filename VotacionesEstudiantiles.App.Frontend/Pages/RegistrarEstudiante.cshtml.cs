93383d58ba025920a12fc20d0d149916f853d6a0
Duvanstp <dizaquita@unal.edu.co>
2022-09-23 09:59:11 -0500

    Version_8.1

---
+++ VotacionesEstudiantiles.App.Frontend/Pages/RegistrarEstudiante.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VotacionesEstudiantiles.App.Pages
+{
+    public class RegistrarEstudianteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
